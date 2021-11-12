2c29653a29c8df2517ba24fa64f168e00b80507e
elik <akainooo@gmail.com>
2021-11-12 16:42:54 +0800

    set form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,21 @@
 class UsersController < ApplicationController
-  
   def sign_up
-
+    @user = user.new
   end
 
   def account_verify
-    redirect_to "/"
+    @user = user.new(clean_params)
+
+    if @user.save
+      redirect_to "/"
+    elsif
+      render :sign_up
+    end
+
+    private
+    def clean_params
+      params.require(:user).permit(:username, :password, :email)
+    end
   end
 
 end

5b875e21c1c7fb7101d24a248f1ee498d01761ff
elik <akainooo@gmail.com>
2021-11-19 18:08:15 +0800

    fixed courses model num error

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,4 @@
 class Course < ApplicationRecord
   validates :name, presence: true
-  validates :price, presence: true, numericality: { greater_than_or_equal_to 0}
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0}
 end

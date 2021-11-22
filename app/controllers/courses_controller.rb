d23b2b4fbf7dfc0e42189778c2e391e5c9aba703
elik <akainooo@gmail.com>
2021-11-22 11:41:00 +0800

    organize code

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,5 +1,8 @@
 class CoursesController < ApplicationController
+  before_action :find_course, only: [:edit, :update, :destroy]
+
   def index
+    @courses = Course.all
   end
 
   def new
@@ -10,13 +13,33 @@ class CoursesController < ApplicationController
     @course = Course.new(course_params)
 
     if @course.save
-      redirect_to courses_path
+      redirect_to courses_path, notice: "Success"
     else
       render :new
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @course.update(course_params)
+      redirect_to courses_path, notice: "Success"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @course.destroy if @course
+    redirect_to courses_path, notice: "Success"
+  end
+
   private
+  def find_course
+    @course = Course.find_by(id: params[:id])
+  end
+
   def course_params
     params.require(:course).permit(:name, :price, :intro, :hour)
   end
